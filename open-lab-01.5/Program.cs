888c11c07fbce5ab396a55dd1a1fbdb8022f0b7a
vladimirtischler <tischler.vladimir@gmail.com>
2019-10-21 21:57:27 +0200

    open lab

--- open-lab-01.5/Program.cs
+++ open-lab-01.5/Program.cs
@@ -4,14 +4,13 @@ namespace open_lab_01._5
 {
     class Program
     {
-        public static bool Reverse(int a)
+        public static bool Reverse(bool a)
         {
-            return !true;
+            return !a;
         }
         static void Main(string[] args)
         {
-            int ab = 2;
-            Console.WriteLine(Reverse(ab));
+             Console.WriteLine(Reverse(true));
         }
     }
 }
